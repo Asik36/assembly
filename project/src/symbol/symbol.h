b03bc61c9489a3c3f630a0d4a823093769ca6634
Asik36 <102484705+Asik36@users.noreply.github.com>
2025-08-07 11:10:26 +0300

    memmory model added

--- project/src/symbol/symbol.h
+++ project/src/symbol/symbol.h
@@ -1,11 +1,30 @@
 #ifndef SYMBOL_H
 #define SYMBOL_H
 #include <stdio.h>
+#include <string.h>
+
+status symbol_create(   instruction * instruction_table, int instruction_num,
+                        directive   * directive_table  , int directive_num  ,
+                        symbol      * symbol_table                          );
+
+void symbol_assign_memmory_address(instruction * ins);
+
+symbol * symbol_get_by_name(symbol * symbol_table, char * name, int name_length);
+
+void symbol_read_instruction_table(instruction * instruction_table,int instruction_num,symbol * symbol_table);
 
 
-
-
-
-
+uint16_t symbol_get_instruction_offset(instruction * ins);
 
 #endif 
+
+/*
+ * macro m1
+ * inc r2
+ * endm
+ * mov m1, r3
+ * 
+ * 
+ * 
+ *
+ */
