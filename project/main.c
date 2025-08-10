0a3596bb99b0f78a84363c0b915db8ed479e7bcf
Ronen Vol <ronen.vol@gmail.com>
2025-08-10 13:56:31 +0300

    finished the conversion of the instructions into the memory as machine code

--- project/main.c
+++ project/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <main.h>
+#include "machine_code.h"
+
 int main(void)
 {
     instruction * instruction_list;
@@ -7,19 +9,19 @@ int main(void)
 
     directive * directive_list;
     int directive_counter = 0;
-    
+
     symbol * symbol_list;
     int symbol_counter = 0;
 
     machine_code * machine_code_list;
     int machine_code_counter = 0;
 
-    
+    machine_code_main(symbol_list)
 
 
 
 
-    return 0;    
+    return 0;
 }
 
 
