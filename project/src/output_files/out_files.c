8150045dcc08b51eab071b468d7f60d90d520192
Ronen Vol <ronen.vol@gmail.com>
2025-08-14 12:53:59 +0300

    finished extern and entry files (before testing)

--- project/src/output_files/out_files.c
+++ project/src/output_files/out_files.c
@@ -2,11 +2,12 @@
 
 #define STARTING_GROUP_NAME 'A'
 
+
 void out_files_main( int machine_code_instructions_length, int machine_code_symbols_length, symbol * symbol_list, int symbol_list_length, char* base_file_name)
 {
     out_files_machine_code(  machine_code_instructions_length,  machine_code_symbols_length, base_file_name);
-    // out_files_extern(  machine_code_instructions_length,  machine_code_symbols_length);
-    // out_files_entry(symbol_list, symbol_list_length);
+    out_files_symbol_files(base_file_name, OUT_FILE_TYPE_EXTERNALS);
+    out_files_symbol_files(base_file_name, OUT_FILE_TYPE_ENTRIES);
 }
 
 static special_base out_files_convert_to_special_base(word_data word)
@@ -49,37 +50,131 @@ static void out_files_write_machine_code_line(FILE * machine_code_f, int word_in
     }
 }
 
+static bool out_files_create_file(out_file_type f_type, char * base_file_name,FILE ** out_file)
+{
+    char ret_file_name [FILE_NAME_LENGTH];
+    char * file_ending = NULL;
+    bool ret = true;
+
+    switch (f_type)
+    {
+    case OUT_FILE_TYPE_OBJECT:
+        file_ending = "ob";
+        break;
+    case OUT_FILE_TYPE_EXTERNALS:
+        file_ending = "ext";
+        break;
+    case OUT_FILE_TYPE_ENTRIES:
+        file_ending = "ent";
+        break;
+
+    default:
+        fprintf(stderr, "%s error: unknown output file type: %d\n",__func__, f_type);
+        ret = false;
+        break;
+    }
+    if(file_ending)
+    {
+        snprintf(ret_file_name , FILE_NAME_LENGTH,"%s.%s", base_file_name,file_ending);
+        *out_file = fopen(ret_file_name,"w");
+    }
+    return(ret);
+}
+
 void out_files_machine_code(  int machine_code_instructions_length, int machine_code_symbols_length, char* base_file_name)
 {
-    char machine_code_file_name [FILE_NAME_LENGTH];
-    snprintf(machine_code_file_name , FILE_NAME_LENGTH,"%s.object", base_file_name);
-    FILE * machine_code_f = fopen(machine_code_file_name, "w");
-    if(! machine_code_f)
-    {
-        fprintf(stderr,"error opening file: %s, in: %s\n",base_file_name, __func__);
-        exit(1);
-        /*add  error handle*/
-    }
 
-    else
+    FILE * machine_code_f ;
+    if(out_files_create_file(OUT_FILE_TYPE_OBJECT, base_file_name, &machine_code_f))
     {
-        /*file header: the length of instructions in words then the length of symbols in words (in the memory)*/
-        fprintf(machine_code_f, "%d\t%d\n", machine_code_instructions_length, machine_code_symbols_length);
-
-        /*file content: each line has 4 digits with the number of word in the memorry, then the name of the special group of 4 bits, and the 4 bits in hexadecimal representation, seperated by '-'*/
-        for(int word_index = STARTING_MEMORY_ADDRESS; word_index < STARTING_MEMORY_ADDRESS + machine_code_instructions_length + machine_code_symbols_length; word_index++)
+        if(! machine_code_f)
         {
-            out_files_write_machine_code_line(machine_code_f, word_index);
+            fprintf(stderr,"error opening file: %s, in: %s\n",base_file_name, __func__);
+            exit(1);
+            /*add  error handle*/
         }
 
-        fclose(machine_code_f);
+        else
+        {
+            /*file header: the length of instructions in words then the length of symbols in words (in the memory)*/
+            fprintf(machine_code_f, "%d\t%d\n", machine_code_instructions_length, machine_code_symbols_length);
+
+            /*file content: each line has 4 digits with the number of word in the memorry, then the name of the special group of 4 bits, and the 4 bits in hexadecimal representation, seperated by '-'*/
+            for(int word_index = STARTING_MEMORY_ADDRESS; word_index < STARTING_MEMORY_ADDRESS + machine_code_instructions_length + machine_code_symbols_length; word_index++)
+            {
+                out_files_write_machine_code_line(machine_code_f, word_index);
+            }
+
+            fclose(machine_code_f);
+        }
     }
 }
-// void out_files_extern(int machine_code_instructions_length, int machine_code_symbols_length)
-// {
-//     /*code*/
-// }
-// void out_files_entry( symbol * symbol_list, int symbol_list_length)
-// {
-//     /*CODE*/
-// }
+
+void out_files_write_line(FILE * out_file, out_file_type file_type, int symbol_index)
+{
+    symbol_call symbol_to_write;
+    switch (file_type)
+    {
+    case OUT_FILE_TYPE_ENTRIES:
+        symbol_to_write = g_entrys[symbol_index];
+        fprintf(out_file, "%s,%d,%d\n",symbol_to_write.symbol_name, (symbol_to_write.base_address / 16) * 16, symbol_to_write.base_address % 16);
+        break;
+    case OUT_FILE_TYPE_EXTERNALS:
+        symbol_to_write = g_externals[symbol_index];
+        fprintf(out_file, "%s BASE %d\n", symbol_to_write.symbol_name, symbol_to_write.base_address);
+        fprintf(out_file, "%s OFFSET %d\n\n", symbol_to_write.symbol_name, symbol_to_write.base_address + 1);
+        break;
+    }
+
+
+}
+
+void out_files_symbol_files(char * base_file_name, out_file_type file_type)
+{
+    int length = 0;
+
+    switch (file_type)
+    {
+    case OUT_FILE_TYPE_ENTRIES:
+        length = g_entry_defenition_length;
+        break;
+    case OUT_FILE_TYPE_EXTERNALS:
+        length = g_extern_call_length;
+        break;
+    default:
+        fprintf(stderr, "%s error: unknown output file type: %d\n",__func__, file_type);
+        break;
+    }
+
+    if(length)
+    {
+        FILE * out_file;
+        if( out_files_create_file(file_type, base_file_name, &out_file))
+        {
+            if(!out_file)
+            {
+                char * file_type_name;
+                if(file_type == OUT_FILE_TYPE_EXTERNALS)
+                {
+                    file_type_name = "externals";
+                }
+                else
+                {
+                    file_type_name = "entries";
+                }
+
+                fprintf(stderr,"%s error: opening %s file\n", __func__, file_type_name);
+            }
+            else
+            {
+                for(int call_index = 0; call_index < length; call_index++)
+                {
+                    out_files_write_line(out_file, file_type, call_index);
+                }
+                fclose(out_file)
+            }
+        }
+    }
+
+}
+
--- project/src/output_files/out_files.h
+++ project/src/output_files/out_files.h
@@ -18,13 +18,17 @@ typedef struct special_base_s
     special_bits_group groups[AMOUNT_OF_SPETIAL_BITS_GROUPS];
 } special_base;
 
+typedef enum out_filetype_e
+{
+    OUT_FILE_TYPE_OBJECT,
+    OUT_FILE_TYPE_EXTERNALS,
+    OUT_FILE_TYPE_ENTRIES
+}out_file_type;
 
 void out_files_main(int machine_code_instructions_length, int machine_code_symbols_length, symbol * symbol_list, int symbol_list_length, char* base_file_name);
 
 void out_files_machine_code(int machine_code_instructions_length, int machine_code_symbols_length, char* base_file_name);
 
-void out_files_extern(int machine_code_instructions_length, int machine_code_symbols_length);
-
-void out_files_entry( symbol * symbol_list, int symbol_list_length);
+void out_files_symbol_files(char * base_file_name, out_file_type file_type);
 
 #endif
